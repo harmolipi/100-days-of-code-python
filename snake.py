9e50212b7ab1ebe6a656da38653e4a635a0859c8
Niko Birbilis <nikob381@gmail.com>
2024-07-06 22:36:26 -0400

    feat(snake): display high score

--- snake.py
+++ snake.py
@@ -53,3 +53,10 @@ class Snake:
 
     def extend(self) -> None:
         self.add_segment(self.segments[-1].position())
+
+    def reset(self) -> None:
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
