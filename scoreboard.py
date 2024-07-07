d128c666c34adddafdc830f5aee60442b4333c7c
Niko Birbilis <nikob381@gmail.com>
2024-07-06 22:36:41 -0400

    feat(snake): save and load high score from file

--- scoreboard.py
+++ scoreboard.py
@@ -8,12 +8,13 @@ class Scoreboard(Turtle):
     def __init__(self) -> None:
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.load_high_score()
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
+        self.load_high_score()
 
     def update_scoreboard(self):
         self.clear()
@@ -38,3 +39,14 @@ class Scoreboard(Turtle):
             self.save_high_score()
         self.score = 0
         self.update_scoreboard()
+
+    def load_high_score(self) -> int:
+        with open("data.txt") as f:
+            contents = f.read()
+            if contents == "":
+                return 0
+            return int(contents)
+
+    def save_high_score(self) -> None:
+        with open("data.txt", mode="w") as f:
+            f.write(str(self.high_score))
