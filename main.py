8fbf978b429d19a6b7008ccaca894d7d886c0ef0
Niko Birbilis <nikob381@gmail.com>
2024-07-08 02:50:21 -0400

    feat(50 states): save missed states to a csv file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import turtle
 import pandas
 
 states_data = pandas.read_csv("./50_states.csv")
-lowercase_states = states_data["state"].map(lambda state: str.lower(state))
+lowercase_states = states_data["state"]
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -24,15 +24,15 @@ game_is_on = True
 
 
 def plot_answer(state: pandas.Series):
-    x = int(state.x)
-    y = int(state.y)
+    x = int(state.x.item())
+    y = int(state.y.item())
     state_name = state.state.item()
 
     answers_turtle.goto((x, y))
     answers_turtle.write(state_name)
 
 
-while game_is_on:
+while len(correct_answers) < 50:
     answer_state = screen.textinput(
         f"{len( correct_answers )}/50 States Correct", "What's another state name?"
     )
@@ -41,6 +41,9 @@ while game_is_on:
         continue
 
     answer_state = answer_state.title()
+    if answer_state == "Exit":
+        break
+
     found_state = states_data[states_data.state == answer_state]
 
     if answer_state in correct_answers:
@@ -50,4 +53,11 @@ while game_is_on:
         correct_answers.append(answer_state)
         plot_answer(found_state)
 
-screen.exitonclick()
+states_to_learn = {"states to learn": []}
+for row in states_data.iterrows():
+    state = row[1].state
+    if state not in correct_answers:
+        states_to_learn["states to learn"].append(state)
+
+states_to_learn_df = pandas.DataFrame(states_to_learn)
+states_to_learn_df.to_csv("states_to_learn.csv")
